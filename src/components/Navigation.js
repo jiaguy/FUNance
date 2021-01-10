636c0327eb4f49a8466ea7785d439e8f245b8f6b
Jiaying Li <jiaguy@users.noreply.github.com>
2021-01-10 02:19:22 -0500

    ajef (#30)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import logotext from '../resources/logotext.svg';
+import React, {useState} from 'react';
+
 // import bootstrap components here
-import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
+import Stats from './Stats';
 
 const Navigation = () => {
+
     return (
         <div>
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/"><img src={logotext} width="100px" alt="logo" /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/about">About</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="/age10">Age 10</NavDropdown.Item>
-                        <NavDropdown.Item href="/age11">Age 11</NavDropdown.Item>
-                        <NavDropdown.Item href="/age12">Age 12</NavDropdown.Item>
-                        <NavDropdown.Item href="/age13">Age 13</NavDropdown.Item>
-                        <NavDropdown.Item href="/age14">Age 14</NavDropdown.Item>
-                        <NavDropdown.Item href="/age15">Age 15</NavDropdown.Item>
+                <Navbar.Brand href="/">Funance</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/age10">Age 10</NavDropdown.Item>
+                            <NavDropdown.Item href="/age11">Age 11</NavDropdown.Item>
+                            <NavDropdown.Item href="/age12">Age 12</NavDropdown.Item>
+                            <NavDropdown.Item href="/age13">Age 13</NavDropdown.Item>
+                            <NavDropdown.Item href="/age14">Age 14</NavDropdown.Item>
+                            <NavDropdown.Item href="/age15">Age 15</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <NavDropdown title="Stats" id="basic-nav-dropdown" style={{ width: '200px' }}>
+                        <NavDropdown.Item>
+                        <Stats
+                            Name="Helen"
+                            JobDesc="Placeholder"
+                            CurrBalance="Placeholder"/>
+                        </NavDropdown.Item>
                     </NavDropdown>
-                </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>
+                </Navbar.Collapse>
             </Navbar>
         </div>
     );
