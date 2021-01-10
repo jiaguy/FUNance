3ee41f2918c59314aa41df3883033d25822fd873
ylou33 <75289961+ylou33@users.noreply.github.com>
2021-01-10 01:01:36 -0500

    Update About.js
    added card format

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,33 @@
 import React from 'react';
-   
-const About = () => {
-    return (
-       <div>
-          <h1>About US</h1>
-          <p>We are a team of four young women who aim to teach young girls ages 10-15 the financial literacy skills they need to save early and spend wisely.</p>
-          <p>This program will introduce a series of informational content catered to each age group and activites that will test financial knowledge.</p>
-          <p>To start the journey, please head to "Home".</p>
-          <p>Happy learning!</p>
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 
-       </div>
-    );
+const About = () => {
+   return (
+
+      <div>
+         {
+
+            <CardDeck className="Card-deck">
+
+               <Card id="Card">
+                  <Card.Body>
+                     <Card.Title>About Us</Card.Title>
+                     <Card.Text>
+
+                        <p>We are a team of four young women who aim to teach young girls ages 10-15 the financial literacy skills they need to save early and spend wisely.</p>
+                        <p>This program will introduce a series of informational content catered to each age group and activites that will test financial knowledge.</p>
+                        <p>To start the journey, please head to "Home".</p>
+                        <p>Happy learning!</p>
+
+                     </Card.Text>
+                  </Card.Body>
+               </Card>
+
+            </CardDeck>
+         }
+      </div >
+   );
 }
- 
+
 export default About;
