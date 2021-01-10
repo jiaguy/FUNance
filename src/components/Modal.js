423e10570b08fbd607cf68635108371e47b0f722
Jiaying Li <jiaguy@users.noreply.github.com>
2021-01-10 00:46:59 -0500

    Made modal div and complete button center (#25)

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,40 +6,40 @@ import Modal from 'react-bootstrap/Modal';
 
 
 function ModalBox(props) {
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          Complete
-        </Button>
-  
-        <Modal
-          show={show}
-          onHide={handleClose}
-          backdrop="static"
-          keyboard={false}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Congratulations</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            Kudos to you for completing this stage of the activity! You will
-            now move onto the next stage.
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div className="Modal-div">
+      <Button variant="primary" onClick={handleShow}>
+        Complete
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Congratulations</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Kudos to you for completing this stage of the activity! You will
+          now move onto the next stage.
           </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" href={props.NextPage}>
-                Next
+        <Modal.Footer>
+          <Button variant="primary" href={props.NextPage}>
+            Next
             </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
-  
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
 //   render(<Example />);
 
 export default ModalBox;
