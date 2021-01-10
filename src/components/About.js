b24cdbc38bc6b4302e7a8cba107a46c23cc17094
ylou33 <75289961+ylou33@users.noreply.github.com>
2021-01-09 23:28:15 -0500

    Update About.js
    fluff

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,10 @@ const About = () => {
     return (
        <div>
           <h1>About US</h1>
-          <p>About US page body content</p>
+          <p>We are a team of four young women who aim to teach young girls ages 10-15 the financial literacy skills they need to save early and spend wisely.</p>
+          <p>This program will introduce a series of informational content catered to each age group and activites that will test financial knowledge.</p>
+          <p>To start the journey, please select "Age 10" the dropdown menu.</p>
+          <p>Happy learning!</p>
 
        </div>
     );
