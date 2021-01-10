69c6437743ab3b1155d6462b8ffe6be532d90148
Jiaying Li <jiaguy@users.noreply.github.com>
2021-01-10 00:15:28 -0500

    Fixed budget layout

--- src/components/Age11.js
+++ src/components/Age11.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ControlledCarousel from './Carousel';
 
 import Budget from './Budget'
-import lawn_mow from '../resources/lawn_mow.jpg';
-import lemonade_stand from '../resources/lemonade_stand.jpg';
-import walk_dog from '../resources/walk_dog.jpg';
-
 import Stats from './Stats';
 
 import budget_slide1 from '../resources/budget_slide1.PNG';
@@ -18,7 +14,6 @@ import budget_slide5 from '../resources/budget_slide5.PNG';
 const Age11 = () => {
     return (
        <div>
-            <Budget />
             <h1>Age 11</h1>
             <div className='Stat-box'>
                 <Stats
@@ -26,14 +21,16 @@ const Age11 = () => {
                 JobNm="Placeholder"
                 CurrBalance="Placeholder"/>
             </div>
+            
             <div className='Slide-deck'>
-            <ControlledCarousel
-            slideImage1={budget_slide1}
-            slideImage2={budget_slide2}
-            slideImage3={budget_slide3}
-            slideImage4={budget_slide4}
-            slideImage5={budget_slide5}/>
+                <ControlledCarousel
+                slideImage1={budget_slide1}
+                slideImage2={budget_slide2}
+                slideImage3={budget_slide3}
+                slideImage4={budget_slide4}
+                slideImage5={budget_slide5}/>
             </div>
+            <Budget/>
        </div>
     );
 }
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -10,58 +10,61 @@ function onSubmit() {
 function Budget() {
     return (
 
-        <div id="Budget">
-            <br></br>
-            <h1>My Monthly Earnings Calculator</h1>
+        <div className="Budget" style={{textAlign: 'center', width: '100%', overflow: 'hidden'}}>
+            <div style={{ width: '40%', float: 'left' }}>
+                <br></br>
+                <h1>My Monthly Earnings Calculator</h1>
 
-            <Form>
-                <Form.Group controlId="formBasicSalary">
-                    <Form.Label>Enter your hourly salary:</Form.Label>
-                    <Form.Control type="salary" placeholder="Enter salary" />
-                </Form.Group>
+                <Form>
+                    <Form.Group controlId="formBasicSalary">
+                        <Form.Label>Enter your hourly salary:</Form.Label>
+                        <Form.Control type="salary" placeholder="Enter salary" />
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicHours">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="hours" placeholder="Enter hours" />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={onSubmit}>
-                    Submit
+                    <Form.Group controlId="formBasicHours">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="hours" placeholder="Enter hours" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={onSubmit}>
+                        Submit
                 </Button>
-            </Form>
+                </Form>
+            </div>
 
-
-            <br></br>
-            <h1>My Budget Worksheet</h1>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Description</th>
-                        <th>Cost</th>
-                        <th>Balance</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>2021/01/09</td>
-                        <td>A second-hand bicycle</td>
-                        <td>-$80</td>
-                        <td>$160</td>
-                    </tr>
-                    <tr>
-                        <td>2021/01/03</td>
-                        <td>Art contest first place prize</td>
-                        <td>+$100</td>
-                        <td>$260</td>
-                    </tr>
-                    <tr>
-                        <td>2021/01/03</td>
-                        <td>Candy</td>
-                        <td>-$2</td>
-                        <td>$258</td>
-                    </tr>
-                </tbody>
-            </Table>
+            <div style={{width: '40%', marginLeft: '620px' }}>
+                <br></br>
+                <h1>My Budget Worksheet</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Description</th>
+                            <th>Cost</th>
+                            <th>Balance</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>2021/01/09</td>
+                            <td>A second-hand bicycle</td>
+                            <td>-$80</td>
+                            <td>$160</td>
+                        </tr>
+                        <tr>
+                            <td>2021/01/03</td>
+                            <td>Art contest first place prize</td>
+                            <td>+$100</td>
+                            <td>$260</td>
+                        </tr>
+                        <tr>
+                            <td>2021/01/03</td>
+                            <td>Candy</td>
+                            <td>-$2</td>
+                            <td>$258</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
         </div>
 
     );
